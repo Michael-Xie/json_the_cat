922e69c362cdc583286546f41c4f1aeaea7ad840
Michael-Xie <xiemicha@gmail.com>
2019-12-05 18:54:27 +0000

    Refactored code to move user-facing code to index.js and preparing to make tests with Mocha Chai

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,22 +1,23 @@
 const request = require('request');
-const args = process.argv;
-const [, , breed] = args;
 
-const searchBreed = "https://api.thecatapi.com/v1/breeds/search?q=" + breed;
+const searchBreed = "https://api.thecatapi.com/v1/breeds/search?q=";
 
-const printSearchResult = function(result) {
-  for (let breed of result) {
-    console.log(`${breed.description}`);
-  }
+const fetchBreedDescription = function (breedName, callback) {
+  request(searchBreed+breedName, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+    } else {
+      let breedInfo = JSON.parse(body);
+      if (breedInfo.length > 0) {
+        callback(null, breedInfo[0].description)
+      } else {
+        callback(`${breedName} not found`, null)
+      } 
+    }
+
+  })
 };
 
-request(searchBreed, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    let breedInfo = JSON.parse(body);
-    breedInfo.length > 0 ? printSearchResult(breedInfo) : console.log(`${breed} not found. Try again.`);
-  }
-});
+module.exports = { fetchBreedDescription };
 
 
