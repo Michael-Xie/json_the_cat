9e102a6237c5105f5164168082d8ddedc72d2e5e
Michael-Xie <xiemicha@gmail.com>
2019-12-05 18:05:10 +0000

    breedFetcher.js is able to take in breed argument from commandline and print out search results normally

---
+++ breedFetcher.js
@@ -0,0 +1,22 @@
+const request = require('request');
+const args = process.argv;
+const [, , breed] = args;
+
+const searchBreed = "https://api.thecatapi.com/v1/breeds/search?q=" + breed;
+
+const printSearchResult = function(result) {
+  for (let breed of result) {
+    console.log(`${breed.description}`);
+  }
+};
+
+request(searchBreed, (error, response, body) => {
+  if (error) {
+    return error;
+  } else {
+    let breedInfo = JSON.parse(body);
+    breedInfo.length > 0 ? printSearchResult(breedInfo) : console.log(`${breed} not found. Try again.`);
+  }
+});
+
+
